29962d5879a5c97eaefd149ed594852c012f4e58
Hotep Ruiz <titoshosito@gmail.com>
2025-06-18 14:51:26 -0600

    intentando resolver problemas con modo oscuro

--- frontend/elementos/topbar.jsx
+++ frontend/elementos/topbar.jsx
@@ -4,18 +4,29 @@ import '../src/index.css'
 
 export const TopBar = ({color, onClick}) => {
   return (
-    <div className={`topbar !bg-${color} !pl-3 !pr-8`}>
+    <div className={`topbar !bg-${color} !pl-3 !pr-8 flex justify-end`}>
       {"hola com estas"}
 
-
-      <button
-        className="p-0 m-0 !bg-transparent border-none"
-        onClick={onClick}
-      >
-        <img src="/imagenes/darkmode.png" className="w-8 h-8" alt="modo oscuro" />
-      </button>
+      <div>
 
 
+        <button
+          className="p-0 m-0 !bg-transparent border-none"
+          onClick={onClick}
+        >
+          <img src="/imagenes/darkmode.png" className="w-8 h-8" alt="modo oscuro" />
+        </button>
+
+        <button
+          className="p-0 m-0 !bg-transparent border-none"
+          onClick={onClick}
+        >
+          <img src="/imagenes/darkmode.png" className="w-8 h-8" alt="modo oscuro" />
+        </button>
+
+
+      </div>
+      
     </div>
   )
 }
